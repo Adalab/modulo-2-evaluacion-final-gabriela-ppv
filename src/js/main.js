1c78134975459621a35bd24e9ff65609362744a6
gabriela-ppv <gabrielapintovas@gmail.com>
2024-01-18 23:36:36 +0100

    change function

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,75 @@
-'use strict';
+"use strict";
+const replacementUrl =
+  "https://www.shutterstock.com/es/image-vector/fantasy-anime-girl-exploring-mystical-forest-2324079729";
+const buttonSearch = document.querySelector(".js-buttonsearch");
+const buttonReset = document.querySelector(".js-buttonreset");
+const listFavorites = document.querySelector(".js-favoriteslist");
+const listResults = document.querySelector(".js-resultslist");
+const inputSearch = document.querySelector(".js-input");
+let seriessearch = "";
+let arraySeries = [];
+let arrayFavorites = [];
 
-console.log('>> Ready :)');
+// funcion que hace la peticion de los datos a API
+function dataApi() {
+  fetch(`https://api.jikan.moe/v4/anime?q=${seriessearch}`)
+    .then((response) => response.json())
+    .then((info) => {
+      arraySeries = info.data;
+      console.log(info.data);
+      renderAnime(arraySeries, listResults); // aqui las mete al array,con el parametro y pinta los resultados
+    });
+}
+// funcion manejadora del evento click de la busqueda general
+function handleclickSearch(event) {
+  event.preventDefault();
+  seriessearch = inputSearch.value;
+  dataApi();
+}
+//evento busqueda boton
+buttonSearch.addEventListener("click", handleclickSearch);
+
+function handleAddFavorites(event) {
+  //funcion manejadora evento click favoritos
+  event.preventDefault();
+  console.log(event.currentTarget.id);
+  const idAnime = event.currentTarget.id;
+  //  console.log (idAnime);
+  const findAnime = arraySeries.find(
+    (animeSelect) => parseInt(animeSelect.mal_id) === parseInt(idAnime)
+  ); //
+
+  const indexAnimeFav = arrayFavorites.findIndex(
+    (animeSelect) => parseInt(idAnime) === parseInt(animeSelect.mal_id)
+  );
+  if (indexAnimeFav === -1) {
+    arrayFavorites.push(findAnime);
+  }
+  renderAnime(arrayFavorites, listFavorites);
+}
+
+function listenerAnime() {
+  // funcion que escucha eventos en los anime y se ejecuta en render al terminar render.
+  const allAnime = document.querySelectorAll(".js-anime");
+  for (const oneAnime of allAnime) {
+    oneAnime.addEventListener("click", handleAddFavorites);
+  }
+}
+
+//funcion que pinta los resultados
+function renderAnime(arrayAnimes, urlrender) {
+  urlrender.innerHTML = ""; // para que no se me acomularan las busquedas y se vaciara.
+  let html = "";
+
+  for (const animes of arrayAnimes) {
+    const imageUrl = animes.images
+      ? animes.images.jpg.image_url
+      : replacementUrl;
+    html += `<li class ="js-anime" id="${animes.mal_id}"> <h5>${animes.title}</h5>
+        <img src="${imageUrl}" alt="foto portada Anime">
+        </li>`;
+  }
+  urlrender.innerHTML += html; //le paso el parametro aqui tambien para que cambie de lugar
+
+  listenerAnime();
+}
